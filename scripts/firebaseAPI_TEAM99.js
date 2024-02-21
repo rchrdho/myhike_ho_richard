59721daf4f63b8de3e31b19863648442d5875aa0
HoRichard <richardho16@hotmail.com>
2024-02-20 19:43:30 -0800

    did a ton of demo stuff with skeleton templates

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyC9TMgj4nU9jU6h5elIPDSmIB1vZzB3DLU",
+    authDomain: "comp1800-demo07-fa4f0.firebaseapp.com",
+    projectId: "comp1800-demo07-fa4f0",
+    storageBucket: "comp1800-demo07-fa4f0.appspot.com",
+    messagingSenderId: "1074446690859",
+    appId: "1:1074446690859:web:72bea4ba65af9d5ab5986e"
 };
 
 //--------------------------------------------
